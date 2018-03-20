502c9b7fc1900e6f62d4077990b428b53b20a086
Mandy Chan <manchan@google.com>
2018-03-19 18:44:03 -0700

    Convert to use client lib v2
    Bug: 75073697
    Change-Id: Ic618f03fe6cade07c48b0b614d7b80b6715308a4

--- functions/index.js
+++ functions/index.js
@@ -13,30 +13,14 @@
 
 'use strict';
 
-process.env.DEBUG = 'actions-on-google:*';
-const { DialogflowApp } = require('actions-on-google');
+const {dialogflow} = require('actions-on-google');
 const functions = require('firebase-functions');
 
-const NAME_ACTION = 'make_name';
-const COLOR_ARGUMENT = 'color';
-const NUMBER_ARGUMENT = 'number';
+const app = dialogflow({debug: true});
 
-exports.sillyNameMaker = functions.https.onRequest((request, response) => {
-  const app = new DialogflowApp({request, response});
-  console.log('Request headers: ' + JSON.stringify(request.headers));
-  console.log('Request body: ' + JSON.stringify(request.body));
-
-  // Make a silly name
-  function makeName (app) {
-    let number = app.getArgument(NUMBER_ARGUMENT);
-    let color = app.getArgument(COLOR_ARGUMENT);
-    app.tell('Alright, your silly name is ' +
-      color + ' ' + number +
-      '! I hope you like it. See you next time.');
-  }
-
-  let actionMap = new Map();
-  actionMap.set(NAME_ACTION, makeName);
-
-  app.handleRequest(actionMap);
+app.intent('make_name', (conv, {color, number}) => {
+  conv.close(`Alright, your silly name is ${color} ${number}! ` +
+    `I hope you like it. See you next time.`);
 });
+
+exports.sillyNameMaker = functions.https.onRequest(app);
